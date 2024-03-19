cb04eeb4f8f21d821ac9d29068a688225fec2ab2
Leandro <146351917+Leele09@users.noreply.github.com>
2024-03-19 15:25:45 +0100

    add backend

--- src/data/serveur/server.mjs
+++ src/data/serveur/server.mjs
@@ -1,18 +1,31 @@
 import { createServer } from "node:http";
-import { getCsv } from './../functions/getcsv.js';
+import * as API from '../API/functions.mjs';
 
 createServer(async (req, res) => {
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    const endpoint = `${req.method}:${url.pathname}`;
-    switch (endpoint){
-        case 'GET:/dataByDate':
-            const date = res.body;
-            //const data = await getCsv(date);
-            console.log('req');
-            break;
-        default:
-            res.writeHead(404);
+    try {
+        let url = new URL(req.url, `http://${req.headers.host}`);
+        const endpoint = `${req.method}:${url.pathname}`;
+        let results;
+        let params;
+        switch (endpoint){
+            case 'GET:/dataByDate':
+                url = new URL(req.url, `http://${req.headers.host}`);
+                params = url.searchParams;
+                results = await API.getCsv(params.get('trip-start'));
+                break;
+            case 'GET:/dataByPolluant':
+                url = new URL(req.url, `http://${req.headers.host}`);
+                params = url.searchParams;
+                results = await API.getDataByParam(params.get('trip-start'));
+                break;
+            default:
+                res.writeHead(404);
+        }
+        if (results) {
+            res.write(JSON.stringify(results));
+        }
+    } catch (e) {
+        throw e;
     }
-
     res.end();
 }).listen(3001)

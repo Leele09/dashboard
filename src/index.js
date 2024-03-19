6f07707f4e23d9a05facb80b722abc990c02fc34
Damien Sabau <83125979+damiensabau@users.noreply.github.com>
2024-03-19 14:37:52 +0100

    add design base

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-const XmlDataComponent = () => {
-    return (
-        <div>
-            <h2>Hey</h2>
-            <form action="http://localhost:3001/dataByDate">
-                <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31"/>
-                <button type={"submit"}>Envoyer</button>
-            </form>
-        </div>
-    );
-};
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<XmlDataComponent/>);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
